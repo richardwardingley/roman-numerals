3daf22adc6a75a998a1858b30440a31d2c1fbd2f
richard.wardingley <richard.wardingley@engineering.digital.dwp.gov.uk>
2021-07-16 09:31:30 +0100

    add test cases

--- src/__test__/romanNumerals.test.js
+++ src/__test__/romanNumerals.test.js
@@ -44,6 +44,54 @@ describe("From numbers to roman numerals function", () => {
         expect(toRoman('13')).toEqual('XIII');
     });
 
+    it('should convert number 19 to IXX', () => {
+        expect(toRoman('19')).toEqual('XIX');
+    });
+
+    it('should convert tens column correctly', () => {
+        expect(toRoman('20')).toEqual('XX');
+        expect(toRoman('40')).toEqual('XL');
+        expect(toRoman('50')).toEqual('L');
+        expect(toRoman('80')).toEqual('LXXX');
+        expect(toRoman('90')).toEqual('XC');
+    });
+
+    it('should convert different 2 digit numbers', () => {
+        expect(toRoman('27')).toEqual('XXVII');
+        expect(toRoman('49')).toEqual('XLIX');
+        expect(toRoman('61')).toEqual('LXI');
+        expect(toRoman('74')).toEqual('LXXIV');
+        expect(toRoman('93')).toEqual('XCIII');
+    });
+
+    it('should convert hundreds column correctly', () => {
+        expect(toRoman('100')).toEqual('C');
+        expect(toRoman('400')).toEqual('CD');
+        expect(toRoman('500')).toEqual('D');
+        expect(toRoman('700')).toEqual('DCC');
+        expect(toRoman('900')).toEqual('CM');
+    });
+
+    it('should convert different 3 digit numbers', () => {
+        expect(toRoman('327')).toEqual('CCCXXVII');
+        expect(toRoman('450')).toEqual('CDL');
+        expect(toRoman('681')).toEqual('DCLXXXI');
+        expect(toRoman('734')).toEqual('DCCXXXIV');
+        expect(toRoman('888')).toEqual('DCCCLXXXVIII');
+    });
+
+    it('should convert thousands column correctly', () => {
+        expect(toRoman('1000')).toEqual('M');
+        expect(toRoman('2000')).toEqual('MM');
+        expect(toRoman('3000')).toEqual('MMM');
+    });
+
+    it('should convert different 4 digit numbers', () => {
+        expect(toRoman('1234')).toEqual('MCCXXXIV');
+        expect(toRoman('2050')).toEqual('MML');
+        expect(toRoman('2985')).toEqual('MMCMLXXXV');
+    });
+
 });
 
 
